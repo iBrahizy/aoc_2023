841bd681d33e2c6eb29662d2ea2b60b02a6daa42
iBrahizy <imahmood1710@gmail.com>
2023-12-25 11:58:34 +0000

    day seven part 2!

--- day_seven/src/main.rs
+++ day_seven/src/main.rs
@@ -2,6 +2,7 @@ use std::cmp::Ordering;
 
 fn main() {
     dbg!(part_one());
+    dbg!(part_two());
 }
 
 fn part_one() -> u32 {
@@ -14,13 +15,101 @@ fn part_one() -> u32 {
     }
 
     hands.sort_by(|x, y| x.0.cmp(&y.0));
-	dbg!(&hands);
 
     hands
         .into_iter()
         .map(|x| x.1)
         .enumerate()
         .fold(0, |acc, (i, x)| acc + ((i as u32 + 1) * x))
+}
+
+fn part_two() -> u32 {
+    let mut hands = Vec::new();
+
+    for line in include_str!("../input.txt").lines() {
+        let (cards, bid) = line.split_once(' ').unwrap();
+
+        hands.push((Hand::new(cards), bid.parse::<u32>().unwrap()));
+    }
+
+    hands.sort_by(|x, y| part_two_cmp(&x.0, &y.0));
+
+    hands
+        .into_iter()
+        .map(|x| x.1)
+        .enumerate()
+        .fold(0, |acc, (i, x)| acc + ((i as u32 + 1) * x))
+}
+
+fn part_two_cmp(hand: &Hand, other_hand: &Hand) -> Ordering {
+    let hand_map = HandMap::new(hand);
+	let hand_j_count = hand_map.map[(11 - Card::MIN) as usize];
+    let other_hand_map = HandMap::new(other_hand);
+	let other_hand_j_count = other_hand_map.map[(11 - Card::MIN) as usize];
+
+    // finds the highest 2, idk i feel like this is a stupid way to do it
+    let mut hand_maxes = hand_map.into_iter().fold([0, 0], |mut acc, (card_type, e)| {
+		// joker
+		if card_type == 11 {
+			return acc;
+		}
+        if e > acc[0] {
+            acc[1] = acc[0];
+            acc[0] = e;
+        } else if e > acc[1] {
+            acc[1] = e;
+        }
+        acc
+    });
+
+	hand_maxes[0] += hand_j_count;
+
+    let mut other_hand_maxes = other_hand_map.into_iter().fold([0, 0], |mut acc, (card_type, e)| {
+		// joker
+		if card_type == 11 {
+			return acc;
+		}
+        if e > acc[0] {
+            acc[1] = acc[0];
+            acc[0] = e;
+        } else if e > acc[1] {
+            acc[1] = e;
+        }
+        acc
+    });
+
+	other_hand_maxes[0] += other_hand_j_count;
+
+    if hand_maxes[0] == other_hand_maxes[0] {
+        let comparison = hand_maxes[1].cmp(&other_hand_maxes[1]);
+
+        if comparison != Ordering::Equal {
+            comparison
+        } else {
+			for (yeppers, other) in hand.cards.into_iter().zip(other_hand.cards) {
+				let yeppers = if yeppers.value == 11 {
+					1
+				} else {
+					yeppers.value
+				};
+
+				let other = if other.value == 11 {
+					1
+				} else {
+					other.value
+				};
+
+				match yeppers.cmp(&other) {
+					Ordering::Equal => (),
+					x => return x,
+				};
+			}
+
+			unreachable!();
+        }
+    } else {
+        hand_maxes[0].cmp(&other_hand_maxes[0])
+    }
 }
 
 #[derive(Clone, Copy, PartialEq, Eq, Debug, PartialOrd, Ord)]
@@ -84,47 +173,37 @@ impl std::cmp::Ord for Hand {
         let self_map = HandMap::new(self);
         let other_map = HandMap::new(other);
 
-        let mut self_maxes = self_map.map.iter().fold([0, 0], |mut acc, e| {
-            let min = acc.iter_mut().min().unwrap();
-
-            if *e > *min {
-                *min = *e;
+        // finds the highest 2, idk i feel like this is a stupid way to do it
+        let self_maxes = self_map.into_iter().fold([0, 0], |mut acc, (_, e)| {
+            if e > acc[0] {
+                acc[1] = acc[0];
+                acc[0] = e;
+            } else if e > acc[1] {
+                acc[1] = e;
             }
-
             acc
         });
 
-        self_maxes.sort();
-
-        let mut other_maxes = other_map.map.iter().fold([0, 0], |mut acc, e| {
-            let min = acc.iter_mut().min().unwrap();
-
-            if *e > *min {
-                *min = *e;
+        let other_maxes = other_map.into_iter().fold([0, 0], |mut acc, (_, e)| {
+            if e > acc[0] {
+                acc[1] = acc[0];
+                acc[0] = e;
+            } else if e > acc[1] {
+                acc[1] = e;
             }
-
             acc
         });
 
-        other_maxes.sort();
-
-        if self_maxes[1] == other_maxes[1] {
-            let comparison = self_maxes[0].cmp(&other_maxes[0]);
+        if self_maxes[0] == other_maxes[0] {
+            let comparison = self_maxes[1].cmp(&other_maxes[1]);
 
             if comparison != Ordering::Equal {
-                return comparison;
+                comparison
+            } else {
+                self.cards.into_iter().cmp(other.cards.into_iter())
             }
-
-            // for (yeppers, other) in self.cards.into_iter().zip(other.cards.into_iter()) {
-            //     match yeppers.value.cmp(&other.value) {
-            //         Ordering::Equal => (),
-            //         x => return x,
-            //     }
-            // }
-
-			self.cards.into_iter().cmp(other.cards.into_iter())
         } else {
-            self_maxes[1].cmp(&other_maxes[1])
+            self_maxes[0].cmp(&other_maxes[0])
         }
     }
 }
@@ -132,6 +211,7 @@ impl std::cmp::Ord for Hand {
 #[derive(Debug)]
 struct HandMap {
     map: [u8; (Card::MAX - Card::MIN + 1) as usize],
+    i: usize,
 }
 
 impl HandMap {
@@ -142,6 +222,21 @@ impl HandMap {
             map[(card.value - Card::MIN) as usize] += 1;
         }
 
-        HandMap { map }
+        HandMap { map, i: 0 }
+    }
+}
+
+impl Iterator for HandMap {
+    type Item = (u8, u8);
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let e = self.map.get(self.i)?;
+        if *e == 0 {
+            self.i += 1;
+            self.next()
+        } else {
+            self.i += 1;
+            Some((self.i as u8 + Card::MIN - 1, *e))
+        }
     }
 }
