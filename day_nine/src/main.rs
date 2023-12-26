127b551a31dd788f369a57f241a0b476785e6d27
ibrahizy <imahmood1710@gmail.com>
2023-12-26 19:46:13 +0000

    day nine!

--- day_nine/src/main.rs
+++ day_nine/src/main.rs
@@ -1,17 +1,13 @@
 fn main() {
     dbg!(part_one());
+    dbg!(part_two());
 }
 
 fn next_element(sequence: &[i32]) -> i32 {
     if sequence.iter().all(|x| *x == 0) {
         0
     } else {
-        let mut differences = Vec::new();
-
-        for arr in sequence.windows(2) {
-            differences.push(arr[1] - arr[0]);
-        }
-
+        let differences: Box<[_]> = sequence.windows(2).map(|arr| arr[1] - arr[0]).collect();
         sequence.iter().last().unwrap() + next_element(&differences)
     }
 }
@@ -19,6 +15,26 @@ fn next_element(sequence: &[i32]) -> i32 {
 fn part_one() -> i32 {
     include_str!("../input.txt")
         .lines()
-        .map(|x| next_element(&x.split(' ').map(|x| x.parse().unwrap()).collect::<Vec<_>>()))
+        .map(|x| {
+            next_element(
+                &x.split(' ')
+                    .map(|x| x.parse().unwrap())
+                    .collect::<Box<[_]>>(),
+            )
+        })
+        .sum()
+}
+
+fn part_two() -> i32 {
+    include_str!("../input.txt")
+        .lines()
+        .map(|x| {
+            next_element(
+                &x.split(' ')
+                    .map(|x| x.parse().unwrap())
+                    .rev()
+                    .collect::<Box<[_]>>(),
+            )
+        })
         .sum()
 }
